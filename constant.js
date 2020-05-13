c990797c8949cf1d5efb4e1eb493d61e3ed3b4dc
jambal tumurtogoo <tjambal21@gmail.com>
2020-05-14 05:53:38 +0900

    commit

---
+++ constant.js
@@ -0,0 +1,18 @@
+//login info
+exports.user_info = {
+	username: "fuyuno-s@fasic.co.jp",
+	password : "Fuyuno0824"
+}
+
+const path = 'C:\\web-crawler\\';
+
+exports.path = path;
+
+//env info
+exports.envConf = {
+	log_name  : path + 'log.txt',
+	list_item_query : 'div.product-item-info',
+	nedb_file_name : path + 'list-items.nedb',
+	env_time : path + 'env_time.txt',
+}
+
